b26a09a01aabecefe52da7a2a2672043bc6155ce
HCanosa <161522324+HCanosa@users.noreply.github.com>
2024-03-19 15:01:05 -0300

    Update AtividadeB1-3.c

--- Entrega de atividades/AtividadeB1-3.c
+++ Entrega de atividades/AtividadeB1-3.c
@@ -1,3 +1,11 @@
+/*--------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados            */
+/*                         Prof Veríssimo                                   */
+/*             Objetivo: Exercícios envolvendo ponteiros e arrays           */
+/*             Paradigama: Programação Modular                              */
+/*                                                                          */
+/*                                                          Data:18/03/2024 */
+/*--------------------------------------------------------------------------*/
 #include <stdio.h>
 
 #define TAMANHO_ARRAY 5
